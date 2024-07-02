2da26c92190137c120fdf23aa4c6df5c7e6b458a
sarvar2003 <saravarjuraev@gmail.com>
2024-07-02 13:49:23 +0200

    Added Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Bars3BottomLeftIcon } from '@heroicons/react/16/solid'
+import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+        <div className="logo">
+            Eventure
+        </div>
+
+        <div className="searchBar">
+            <Bars3BottomLeftIcon
+                width={20}
+                height={20}
+                color='#49454F'
+            />
+            <input type="search" name="event-search" id="event-search" placeholder='Search events' />
+            <MagnifyingGlassIcon 
+                width={20}
+                height={20}
+                color='#49454F'
+            />
+        </div>
+
+        <div className="navLinks">
+            <ul>
+                <li>Find Events</li>
+                <li>Create Events</li>
+                <li>Login</li>
+                <li>Signup</li>
+            </ul>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
